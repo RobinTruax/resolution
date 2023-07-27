c1db3b94a2e5bbb7fd3090b8d06257eda185a2c7
roshan <roshan@truax.cc>
2023-07-27 13:21:19 -0500

    Polishing (currently broken)

--- lua/filesys/utilities.lua
+++ lua/filesys/utilities.lua
@@ -1,219 +1,124 @@
---[[------------------- resolution v0.1.0 -----------------------
+--[[--------------------------- resolution v0.1.0 ------------------------------
 
-utilities for filesystem menus
+resolution is a Neovim config for writing TeX and doing computation math.
 
----------------------------------------------------------------]]
+This file contains utilities for the file system module in particular.
+
+Copyright (C) 2023 Roshan Truax
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) at any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+------------------------------------------------------------------------------]]
 
 local utilities = {}
 
------------------------------------------------------------------
-
-local pickers = require('telescope.pickers')
-local finders = require('telescope.finders')
-local conf = require('telescope.config').values
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
-local previewers = require('telescope.previewers')
+--------------------------------- dependencies ---------------------------------
 
 local prefs = require('config.preferences')
 local cfg_filesys = require('config.advanced.filesys')
 local states = require('core.states')
+local core_utils = require('core.utilities')
 
-------------------- low-level file utilities --------------------
-
-utilities.file_exists = function(path)
-    local f = io.open(path, "rb")
-    if f then f:close() end
-    return f ~= nil
-end
-
-utilities.string_from_file = function(path)
-    if not utilities.file_exists(path) then return '' end
-    local lines = ''
-    for line in io.lines(path) do
-        lines = lines .. ' ' .. line
-    end
-    return lines
-end
-
-utilities.read_file = function(path)
-    local file = io.open(path, "rb")
-    if not file then return nil end
-    local content = file:read "*a"
-    file:close()
-    return content
-end
-
---------------------- search in directories ---------------------
-
-utilities.get_files_in_directory = function(dir)
-    local files = {}
-    local iterator = nil
-    if vim.g.windows == false then
-        iterator = io.popen('ls -pa ' .. dir .. ' | grep -v /')
-    elseif vim.g.windows == true then
-        iterator = io.popen('dir "' .. dir .. '" /b')
-    else
-        error('Windows variable has unrecognized value in function which makes system call.')
-    end
-    for file in iterator:lines() do
-        table.insert(files, dir .. file)
-    end
-    return files
-end
-
-utilities.get_subdirs_in_directory = function(dir)
-    local files = {}
-    local iterator = nil
-    if vim.g.windows == false then
-        iterator = io.popen('ls -d ' .. dir .. '*')
-    elseif vim.g.windows == true then
-        error('Not implemented yet.')
-    else
-        error('Windows variable has unrecognized value in function which makes system call.')
-    end
-    for file in iterator:lines() do
-        table.insert(files, file)
-    end
-    return files
-end
-
-------------------- system calls split by os --------------------
-
-utilities.copy_file = function(oldfile, newfile)
-    if vim.g.windows == false then
-        vim.fn.system(string.format('cp %s %s', oldfile, newfile))
-    elseif vim.g.windows == true then
-        vim.fn.system(string.format('copy %s %s', oldfile, newfile))
-    else
-        error('Windows variable has unrecognized value in function which makes system call.')
-    end
-end
-
-utilities.make_dir = function(location)
-    if vim.g.windows == false then
-        vim.fn.system(string.format('mkdir %s', location))
-    elseif vim.g.windows == true then
-        error('Not implemented yet.')
-    else
-        error('Windows variable has unrecognized value in function which makes system call.')
-    end
-end
-
-utilities.mv_folder = function(oldloc, newloc)
-    if vim.g.windows == false then
-        vim.fn.system(string.format('mv %s %s', oldloc, newloc))
-    elseif vim.g.windows == true then
-        error('Not implemented yet.')
-    else
-        error('Windows variable has unrecognized value in function which makes system call.')
-    end
-end
-
---------------------- manipulate file path ----------------------
-
-utilities.trim_path_dir = function(path)
-    return vim.fn.fnamemodify(path, ':h')
-end
-
-utilities.trim_path_file = function(path)
-    return vim.fn.fnamemodify(path, ':t')
-end
-
-utilities.write_table_to_file = function(table, path)
-    vim.fn.writefile({ vim.json.encode(table) }, path)
-end
-
-utilities.cut_path_to_project = function(path)
-    local root = prefs.project_root_path:gsub('(.-)[\\/]+$', '%1')
-    if path:match(root) == nil then
-        return nil
-    end
-    local _, x = path:find(root)
-    _, x = path:find('[\\/]', x + 1)
-    if x == nil then return nil end
-    _, x = path:find('[\\/]', x + 1)
-    if x == nil then return nil end
-    _, x = path:find('[\\/]', x + 1)
-    if x == nil then x = path:len() end
-    if path:sub(x, x) == '/' then
-        return path:sub(1, x)
-    else
-        return path:sub(1, x) .. '/'
-    end
-end
-
------------------------------ other -----------------------------
-
-utilities.get_project_name = function(path)
-    local cut_path = utilities.cut_path_to_project(path)
-    if cut_path == nil then
-        return 'Not In Project'
-    end
-    local proj_info = cut_path .. cfg_filesys.project_info_name
-    local proj_info_string = utilities.string_from_file(proj_info)
-    if proj_info_string == nil then
-        return 'Project Name Not Found'
-    end
-    return vim.json.decode(proj_info_string).title
-end
+----------------------------- get most recent file -----------------------------
 
 utilities.most_recent_file = function(path, force)
+    -- either if the data has not been compiled or if the system requests a "force"
     if states.most_recent_files[path] == nil or force == true then
-        local cmd = 'ls -t ' .. path .. '/*.tex | head -n1'
-        local untrimmed = vim.fn.system(cmd)
-        states.most_recent_files[path] = untrimmed:sub(1, -2)
+        if vim.g.windows == false then
+            local cmd = 'ls -t ' .. path .. '/*.tex | head -n1'
+            local untrimmed = vim.fn.system(cmd)
+            states.most_recent_files[path] = untrimmed:sub(1, -2)
+        elseif vim.g.windows == true then
+            error('Not implemented on Windows yet.')
+        else
+            error('Unrecognized operating system.')
+        end
     end
     return states.most_recent_files[path]
 end
 
------------------- searching for project info -------------------
+--------------------------- get project information ----------------------------
 
-utilities.get_all_project_infos = function()
+utilities.get_project_info_filepaths = function()
     local files = {}
     local string_of_files = ''
+
+    -- get list of files
     if vim.g.windows == false then
-        string_of_files = io.popen('find ' ..
-        prefs.project_root_path .. ' -name ' .. "'" .. cfg_filesys.project_info_name .. "'")
+        string_of_files = vim.fn.system(string.format(
+            "find %s -name '%s'",
+            prefs.project_root_path,
+            cfg_filesys.project_info_name
+        ))
     elseif vim.g.windows == true then
-        error('Not implemented yet.')
+        error('Not implemented on Windows yet.')
     else
-        error('Windows variable has unrecognized value in function which makes system call.')
+        error('Unrecognized operating system.')
     end
-    for filename in string_of_files:lines() do
-        local root = prefs.project_root_path:gsub('(.-)[\\/]+$', '%1')
+
+    -- trim list of files
+    for filename in string_of_files:gmatch('[^\r\n]+') do
+        -- get project root
+        local root = core_utils.trim_path_dir(project_root_path)
+        -- get archive folder
         local archive = root .. '/' .. cfg_filesys.archive_project_folder
+        -- remove files in the archive folder
         if filename:find(archive) == nil then
             files[#files + 1] = filename
         end
     end
-    string_of_files:close()
+
+    -- return
     return files
 end
 
+------------------------- compile project information --------------------------
+
 utilities.compile_project_infos = function(force)
-    if force == true then
-        states.project_info_compiled = false
-    end
-    if states.project_info_compiled == false or force == true then
-        local table_of_project_infos = {}
-        for _, project_info in pairs(utilities.get_all_project_infos()) do
-            local success, decoded_project_info = pcall(function()
-                return vim.json.decode(utilities.read_file(project_info))
-            end)
-            if success == true then
-                decoded_project_info['filepath'] = project_info
-                table_of_project_infos[#table_of_project_infos + 1] = decoded_project_info
-            elseif success == false then
-                local category = vim.fn.fnamemodify(project_info, ':p:h:h:t')
-                local project = vim.fn.fnamemodify(project_info, ':h:t')
-                vim.notify('Warning: Project Information for "'.. category ..'/' .. project ..'" is Corrupted', vim.log.levels.WARN)
-            end
+    -- initialization
+    local table_of_project_infos = {}
+
+    -- compilation process
+    for _, project_info in pairs(utilities.get_project_info_filepaths()) do
+        -- attempt to access project discovery information
+        local success, decoded_project_info = pcall(function()
+            return vim.json.decode(core_utils.string_from_file(project_info))
+        end)
+        -- if attempt succeeded
+        if success == true then
+            -- create decoded project info and append to table
+            decoded_project_info['filepath'] = project_info
+            table_of_project_infos[#table_of_project_infos + 1] = decoded_project_info
+        -- if attempt failed
+        elseif success == false then
+            -- get subtitute title
+            local category = vim.fn.fnamemodify(project_info, ':p:h:h:t')
+            local project = vim.fn.fnamemodify(project_info, ':h:t')
+            local title = string.format('%s/%s', category, project)
+            -- create decoded project info and append to table
+            decoded_project_info = { title = title }
+            table_of_project_infos[#table_of_project_infos + 1] = decoded_project_info
+            -- notify user of error
+            vim.notify(
+                string.format('Project Information for "%s" is Corrupted', title), 
+                vim.log.levels.WARN
+            )
         end
-        states.table_of_project_infos = table_of_project_infos
-        states.project_info_compiled = true
     end
-    return states.table_of_project_infos
+
+    -- updating global state variable
+    states.table_of_project_infos = table_of_project_infos
 end
 
+--------------------------------------------------------------------------------
+
 return utilities
+
+--------------------------------------------------------------------------------
